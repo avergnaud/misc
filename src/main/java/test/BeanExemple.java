d8427bb972637bcadc44ff7a524e6b73090b14e6
avergnaud <a.vergnaud@gmail.com>
2016-03-04 17:10:23 +0100

    init

---
+++ src/main/java/test/BeanExemple.java
@@ -0,0 +1,44 @@
+package test;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+public class BeanExemple {
+	
+	private String name;
+	private double weight;
+
+	public BeanExemple(String name, double weight) {
+		super();
+		this.name = name;
+		this.weight = weight;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getWeight() {
+		return weight;
+	}
+
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+
+	@Override
+	public String toString() {
+		return ToStringBuilder.reflectionToString(this,
+				ToStringStyle.SHORT_PREFIX_STYLE);
+	}
+
+	public static void main(String... args) {
+		BeanExemple hippo = new BeanExemple("Harry", 1000);
+		System.out.println(hippo);
+	}
+	
+}
